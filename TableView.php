25082ee6e6e1916eb1fe797854d078ee65850bb1
mnewing <mary_newing@hotmail.com>
2018-06-12 22:12:27 +0100

    Update TableView.php
    comments added to add reuse

--- TableView.php
+++ TableView.php
@@ -8,13 +8,20 @@
 
 <body >
 
-<h1>Manage</h1>
+<h1>View</h1>
+<p>The most basic of code allowing you to connect to the database and fetch all records from a table and display them using a HTML table.</p>
+
+<p>To use this you will need to change the database settings if you are not running against a local database and make sure that database details match you table.</p>
 
 <?php
+	//create the SQL query
 	$query = "select * from person";
+	//make a connecto the database
 	$con = mysqli_connect("localhost", "root", "", "test");
+	//run the query and if is well display the data
 	if($result = mysqli_query($con, $query))  
 	{
+		//check and see how may rows where returned
 		switch (mysqli_num_rows($result)) {
 		  case 0:
 			echo "<h>Sorry we cannot find any clients.</p>";
@@ -40,9 +47,11 @@
 			echo '</table>';
 		} //end switch statment
     } else {
-	  echo '<p>An unexpected error occured and we are unable to continue.</p>';
-	  echo("Error description: " . mysqli_error($con)); //debug only - remove this in release version
+		//an error occured fetch data so report back to the user 
+		echo '<p>An unexpected error occured and we are unable to continue.</p>';
+		echo("Error description: " . mysqli_error($con)); //debug only - remove this in release version
     }
+	//tidy up
 	mysqli_close($con);
 ?>
 
